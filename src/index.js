a7e5ab8deda01b2d96354972b61d038fa96b5080
senxzo <innocentaudu2821@gmail.com>
2025-03-05 14:50:40 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { createRoot } from "react-dom/client"; // Import createRoot
 import App from "./App";
 import "./tailwind.css"; // Import Tailwind CSS
 import ReactDOM from "react-dom";
